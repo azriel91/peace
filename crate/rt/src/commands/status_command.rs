475a6d27a9032d28d91b93257b84fb5f7ae0169c
Azriel Hoh <azriel91@gmail.com>
2022-07-21 07:38:37 +1200

    Improve implementation and add docs to `StatusCommand`.

--- crate/rt/src/commands/status_command.rs
+++ crate/rt/src/commands/status_command.rs
@@ -11,6 +11,21 @@ impl<E> StatusCommand<E>
 where
     E: std::error::Error,
 {
+    /// Runs [`FullSpec`]`::`[`StatusFnSpec`]`::`[`exec`] for each full spec.
+    ///
+    /// At the end of this function, [`Resources`] will be populated with
+    /// [`FullSpecStates`].
+    ///
+    /// If any `StatusFnSpec` needs to read the `State` from a previous
+    /// `FullSpec`, the [`FullSpecStatesRw`] type should be used in
+    /// [`FnSpec::Data`].
+    ///
+    /// [`exec`]: peace_cfg::FnSpec::exec
+    /// [`FnSpec::Data`]: peace_cfg::FnSpec::Data
+    /// [`FullSpec`]: peace_cfg::FullSpec
+    /// [`FullSpecStates`]: peace_resources::FullSpecStates
+    /// [`FullSpecStatesRw`]: peace_resources::FullSpecStatesRw
+    /// [`StatusFnSpec`]: peace_cfg::FullSpec::StatusFnSpec
     pub async fn exec(
         full_spec_graph: &FullSpecGraph<E>,
         resources: &Resources<SetUp>,
@@ -18,7 +33,9 @@ where
         full_spec_graph
             .stream()
             .map(Result::<_, E>::Ok)
-            .try_for_each(|full_spec| async move { full_spec.status_fn_exec(resources).await })
+            .try_for_each_concurrent(None, |full_spec| async move {
+                full_spec.status_fn_exec(resources).await
+            })
             .await
     }
 }
