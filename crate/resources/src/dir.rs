e47919e6588112a6c536d5b898b989034b3567d8
Azriel Hoh <azriel91@gmail.com>
2022-08-27 12:56:25 +1200

    Simplify `.peace` directory structure.

--- crate/resources/src/dir.rs
+++ crate/resources/src/dir.rs
@@ -13,19 +13,9 @@
 //!         |   |- ..
 //!         |   |- CmdExecutionN
 //!         |
-//!         |- StatesDir
-//!         |   |- StatesCurrentDir  # `States` split per item_spec
-//!         |   |   |- StatesCurrentMeta
-//!         |   |   |- State0
-//!         |   |   |- ..
-//!         |   |   |- StateN
-//!         |   |
-//!         |   |- StatesDesiredDir  # `StatesDesired` split per item_spec
-//!         |       |- StatesDesiredMeta
-//!         |       |- StateLogical0
-//!         |       |- ..
-//!         |       |- StateLogicalN
-//!         |
+//!         |- StatesMeta
+//!         |- StatesCurrent
+//!         |- StatesDesired
 //!         |- ProfileInit
 //! ```
 //!
@@ -43,45 +33,26 @@
 //!     |   |   |- 00000004_2022-08-22T08_16_09_clean_dry.yaml
 //!     |   |   |- 00000005_2022-08-22T08_16_29_clean.yaml
 //!     |   |
-//!     |   |- states
-//!     |   |   |- current
-//!     |   |   |   |  # Also store the last fetched time so we can inform the user.
-//!     |   |   |   |  # Should time be stored per item spec, or per invocation?
-//!     |   |   |   |- .meta.yaml
-//!     |   |   |   |- item_spec_a.yaml
-//!     |   |   |   |- item_spec_b.yaml
-//!     |   |   |   |- item_spec_c.yaml
-//!     |   |   |
-//!     |   |   |- desired
-//!     |   |       |- .meta.yaml
-//!     |   |       |- item_spec_a.yaml
-//!     |   |       |- item_spec_b.yaml
-//!     |   |       |- item_spec_c.yaml
+//!     |   |- .meta.yaml  # Store the last fetched time so we can inform the user.
+//!     |   |              # Should time be stored per item spec, or per invocation?
 //!     |   |
 //!     |   |- init.yaml  # Parameters used to initialize this profile
-//!     |                 # We write to this so that each time the user re-`init`s,
-//!     |                 # if they version control it, they can rediscover the
-//!     |                 # states from previous inits, and clean them up.
+//!     |   |             # We write to this so that each time the user re-`init`s,
+//!     |   |             # if they version control it, they can rediscover the
+//!     |   |             # states from previous inits, and clean them up.
+//!     |   |
+//!     |   |- states.yaml
+//!     |   |- states_desired.yaml
 //!     |
 //!     |- profile2
 //!         |- .history
 //!         |   |- 00000000_2022-08-21T20_48_02_init.yaml
 //!         |   |- 00000001_2022-08-21T20_48_07_fetch.yaml
 //!         |
-//!         |- states
-//!         |   |- current
-//!         |   |   |- .meta.yaml
-//!         |   |   |- item_spec_a.yaml
-//!         |   |   |- item_spec_b.yaml
-//!         |   |   |- item_spec_c.yaml
-//!         |   |
-//!         |   |- desired
-//!         |       |- .meta.yaml
-//!         |       |- item_spec_a.yaml
-//!         |       |- item_spec_b.yaml
-//!         |       |- item_spec_c.yaml
-//!         |
+//!         |- .meta.yaml
 //!         |- init.yaml
+//!         |- states.yaml
+//!         |- states_desired.yaml
 //! ```
 
 pub use self::{peace_dir::PeaceDir, profile_dir::ProfileDir, workspace_dir::WorkspaceDir};
