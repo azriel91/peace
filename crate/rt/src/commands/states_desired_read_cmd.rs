cc1cd5f223dbd0544ffd886e0b1484913ab029ac
Azriel Hoh <azriel91@gmail.com>
2022-09-05 21:03:57 +1200

    Add `StatesDesiredReadCmd`.

---
+++ crate/rt/src/commands/states_desired_read_cmd.rs
@@ -0,0 +1,120 @@
+use std::marker::PhantomData;
+
+use peace_cfg::ItemSpecId;
+use peace_resources::{
+    paths::{FlowDir, StatesDesiredFile},
+    resources_type_state::{SetUp, WithStates},
+    states::StatesDesired,
+    type_reg::untagged::TypeReg,
+    Resources,
+};
+use peace_rt_model::{CmdContext, Error, Storage};
+
+/// Reads [`StatesDesired`]s from storage.
+#[derive(Debug)]
+pub struct StatesDesiredReadCmd<E>(PhantomData<E>);
+
+impl<E> StatesDesiredReadCmd<E>
+where
+    E: std::error::Error + From<Error> + Send,
+{
+    /// Reads [`StatesDesired`]s from storage.
+    ///
+    /// Either [`StatesDesiredDiscoverCmd`] or [`StatesDiscoverCmd`] must have
+    /// run prior to this command to read the state.
+    ///
+    /// [`ItemSpec`]: peace_cfg::ItemSpec
+    /// [`StateCurrentFnSpec`]: peace_cfg::ItemSpec::StateCurrentFnSpec
+    /// [`StatesDesiredDiscoverCmd`]: crate::StatesDesiredDiscoverCmd
+    /// [`StatesDiscoverCmd`]: crate::StatesDiscoverCmd
+    pub async fn exec(
+        cmd_context: CmdContext<'_, SetUp, E>,
+    ) -> Result<CmdContext<WithStates, E>, E> {
+        let (workspace, item_spec_graph, mut resources, states_type_regs) =
+            cmd_context.into_inner();
+        let states_desired =
+            Self::exec_internal(&mut resources, states_type_regs.states_desired_type_reg()).await?;
+
+        let resources = Resources::<WithStates>::from((resources, states_desired));
+
+        let cmd_context =
+            CmdContext::from((workspace, item_spec_graph, resources, states_type_regs));
+        Ok(cmd_context)
+    }
+
+    /// Returns the [`StatesDesired`] of all [`ItemSpec`]s if it exists on disk.
+    ///
+    /// [`ItemSpec`]: peace_cfg::ItemSpec
+    pub(crate) async fn exec_internal(
+        resources: &mut Resources<SetUp>,
+        states_desired_type_reg: &TypeReg<ItemSpecId>,
+    ) -> Result<StatesDesired, E> {
+        let states = Self::deserialize_internal(resources, states_desired_type_reg).await?;
+
+        Ok(states)
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    async fn deserialize_internal(
+        resources: &mut Resources<SetUp>,
+        states_desired_type_reg: &TypeReg<ItemSpecId>,
+    ) -> Result<StatesDesired, E> {
+        let flow_dir = resources.borrow::<FlowDir>();
+        let storage = resources.borrow::<Storage>();
+        let states_desired_file = StatesDesiredFile::from(&*flow_dir);
+
+        if !states_desired_file.exists() {
+            return Err(E::from(Error::StatesDesiredDiscoverRequired));
+        }
+
+        let states_desired = storage
+            .read_with_sync_api(
+                "states_desired_file_read".to_string(),
+                &states_desired_file,
+                |file| {
+                    let deserializer = serde_yaml::Deserializer::from_reader(file);
+                    let states_desired = StatesDesired::from(
+                        states_desired_type_reg
+                            .deserialize_map(deserializer)
+                            .map_err(Error::StatesDesiredDeserialize)?,
+                    );
+                    Ok(states_desired)
+                },
+            )
+            .await?;
+        drop(flow_dir);
+        drop(storage);
+
+        resources.insert(states_desired_file);
+
+        Ok(states_desired)
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    async fn deserialize_internal(
+        resources: &mut Resources<SetUp>,
+        states_desired_type_reg: &TypeReg<ItemSpecId>,
+    ) -> Result<StatesDesired, E> {
+        let flow_dir = resources.borrow::<FlowDir>();
+        let storage = resources.borrow::<Storage>();
+        let states_desired_file = StatesDesiredFile::from(&*flow_dir);
+
+        let states_desired_file_str = states_desired_file.to_string_lossy();
+        let states_serialized = storage
+            .get_item(states_desired_file_str.as_ref())?
+            .ok_or(Error::StatesDesiredDiscoverRequired)?;
+        let deserializer = serde_yaml::Deserializer::from_str(&states_serialized);
+        let states_desired = StatesDesired::from(
+            states_desired_type_reg
+                .deserialize_map(deserializer)
+                .map_err(Error::StatesDesiredDeserialize)?,
+        );
+
+        drop(flow_dir);
+        drop(storage);
+
+        resources.insert(states_desired_file);
+
+        Ok(states_desired)
+    }
+}
---
+++ workspace_tests/src/rt/states_desired_read_cmd.rs
@@ -0,0 +1,75 @@
+use peace::{
+    cfg::{flow_id, profile, FlowId, ItemSpec, Profile, State},
+    resources::states::StatesDesired,
+    rt::{StatesDesiredDiscoverCmd, StatesDesiredReadCmd},
+    rt_model::{CmdContext, Error, ItemSpecGraphBuilder, Workspace, WorkspaceSpec},
+};
+
+use crate::{VecCopyError, VecCopyItemSpec};
+
+#[tokio::test]
+async fn reads_states_desired_from_disk_when_present() -> Result<(), Box<dyn std::error::Error>> {
+    let tempdir = tempfile::tempdir()?;
+    let workspace = Workspace::init(
+        WorkspaceSpec::Path(tempdir.path().to_path_buf()),
+        profile!("test_profile"),
+        flow_id!("test_flow"),
+    )
+    .await?;
+    let graph = {
+        let mut graph_builder = ItemSpecGraphBuilder::<VecCopyError>::new();
+        graph_builder.add_fn(VecCopyItemSpec.into());
+        graph_builder.build()
+    };
+
+    // Write desired states to disk.
+    let cmd_context = CmdContext::init(&workspace, &graph).await?;
+    let CmdContext {
+        resources: resources_from_discover,
+        ..
+    } = StatesDesiredDiscoverCmd::exec(cmd_context).await?;
+
+    // Re-read states from disk in a new set of resources.
+    let cmd_context = CmdContext::init(&workspace, &graph).await?;
+    let CmdContext {
+        resources: resources_from_read,
+        ..
+    } = StatesDesiredReadCmd::exec(cmd_context).await?;
+
+    let states_from_discover = resources_from_discover.borrow::<StatesDesired>();
+    let vec_copy_state_from_discover =
+        states_from_discover.get::<State<Vec<u8>, ()>, _>(&VecCopyItemSpec.id());
+    let states_from_read = resources_from_read.borrow::<StatesDesired>();
+    let vec_copy_state_from_read =
+        states_from_read.get::<State<Vec<u8>, ()>, _>(&VecCopyItemSpec.id());
+    assert_eq!(vec_copy_state_from_discover, vec_copy_state_from_read);
+    Ok(())
+}
+
+#[tokio::test]
+async fn returens_error_when_states_not_on_disk() -> Result<(), Box<dyn std::error::Error>> {
+    let tempdir = tempfile::tempdir()?;
+    let workspace = Workspace::init(
+        WorkspaceSpec::Path(tempdir.path().to_path_buf()),
+        profile!("test_profile"),
+        flow_id!("test_flow"),
+    )
+    .await?;
+    let graph = {
+        let mut graph_builder = ItemSpecGraphBuilder::<VecCopyError>::new();
+        graph_builder.add_fn(VecCopyItemSpec.into());
+        graph_builder.build()
+    };
+
+    // Try and read desired states from disk.
+    let cmd_context = CmdContext::init(&workspace, &graph).await?;
+    let exec_result = StatesDesiredReadCmd::exec(cmd_context).await;
+
+    assert!(matches!(
+        exec_result,
+        Err(VecCopyError::PeaceRtError(
+            Error::StatesDesiredDiscoverRequired
+        ))
+    ));
+    Ok(())
+}
