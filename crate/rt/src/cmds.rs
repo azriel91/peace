b8589092ec1a018fdfda6bd4cbc0d40fb41162cb
Azriel Hoh <azriel91@gmail.com>
2022-09-18 15:04:14 +1200

    Add documentation in `cmds` and `cmds::sub` modules.

--- crate/rt/src/cmds.rs
+++ crate/rt/src/cmds.rs
@@ -1,3 +1,13 @@
+//! Commands to provide user-facing output.
+//!
+//! Commands directly underneath this module write to the `OutputWrite`
+//! provided in the [`CmdContext`]. Commands at this level are intended to
+//! provide the information requested to the user; errors should inform the user
+//! of any steps that have to be run before the command is able to fulfill the
+//! information request.
+//!
+//! [`CmdContext`]: crate::CmdContext
+
 pub use self::{
     diff_cmd::DiffCmd, ensure_cmd::EnsureCmd, states_current_display_cmd::StatesCurrentDisplayCmd,
     states_desired_display_cmd::StatesDesiredDisplayCmd, states_discover_cmd::StatesDiscoverCmd,
--- crate/rt/src/cmds/sub.rs
+++ crate/rt/src/cmds/sub.rs
@@ -1,3 +1,10 @@
+//! Common logic that is useful as part of a user facing command.
+//!
+//! Commands in this module don't write to [`OutputWrite`], but the provided
+//! logic is useful to build user facing commands.
+//!
+//! [`OutputWrite`]: peace_rt_model_core::OutputWrite
+
 pub use self::{
     states_current_discover_cmd::StatesCurrentDiscoverCmd,
     states_current_read_cmd::StatesCurrentReadCmd,
