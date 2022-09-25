5593488c4e4dc819df593cad8a3fcd0553e0acd0
Azriel Hoh <azriel91@gmail.com>
2022-09-25 15:34:42 +1300

    Add tests for `CmdContextBuilder`.

--- workspace_tests/src/rt_model/cmd_context_builder.rs
+++ workspace_tests/src/rt_model/cmd_context_builder.rs
@@ -2,7 +2,11 @@ use std::path::Path;
 
 use peace::{
     cfg::{flow_id, profile, FlowId, Profile},
-    rt_model::{CmdContextBuilder, ItemSpecGraphBuilder, Workspace, WorkspaceSpec},
+    resources::internal::{FlowInitFile, ProfileInitFile, WorkspaceInitFile},
+    rt_model::{
+        CmdContext, CmdContextBuilder, ItemSpecGraph, ItemSpecGraphBuilder, Workspace,
+        WorkspaceSpec,
+    },
 };
 
 use crate::{no_op_output::NoOpOutput, VecCopyError, VecCopyItemSpec};
@@ -10,19 +14,9 @@ use crate::{no_op_output::NoOpOutput, VecCopyError, VecCopyItemSpec};
 #[tokio::test]
 async fn build_initializes_dirs_using_profile_and_physically_creates_dirs()
 -> Result<(), Box<dyn std::error::Error>> {
-    let tempdir = tempfile::tempdir()?;
-    let workspace = Workspace::new(
-        WorkspaceSpec::Path(tempdir.path().into()),
-        profile!("test_profile"),
-        flow_id!("test_flow"),
-    )?;
-    let graph = {
-        let mut graph_builder = ItemSpecGraphBuilder::<VecCopyError>::new();
-        graph_builder.add_fn(VecCopyItemSpec.into());
-        graph_builder.build()
-    };
-    let mut no_op_output = NoOpOutput;
-    CmdContextBuilder::new(&workspace, &graph, &mut no_op_output).await?;
+    let (tempdir, workspace, graph, mut output) = test_setup().await?;
+
+    CmdContextBuilder::new(&workspace, &graph, &mut output).await?;
 
     let workspace_dirs = workspace.dirs();
     let workspace_dir = tempdir.path();
@@ -49,4 +43,159 @@ async fn build_initializes_dirs_using_profile_and_physically_creates_dirs()
     .iter()
     .for_each(|dir| assert!(dir.exists()));
     Ok(())
+}
+
+#[tokio::test]
+async fn build_inserts_workspace_init_params_from_parameter()
+-> Result<(), Box<dyn std::error::Error>> {
+    let (_tempdir, workspace, graph, mut output) = test_setup().await?;
+
+    let CmdContext { resources, .. } = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_workspace_init(Some("workspace_init".to_string()))
+        .await?;
+    let workspace_init_file = resources.borrow::<WorkspaceInitFile>();
+    let workspace_init = resources.borrow::<String>();
+
+    assert!(workspace_init_file.exists());
+    assert_eq!(
+        "workspace_init\n",
+        tokio::fs::read_to_string(&*workspace_init_file).await?
+    );
+    assert_eq!("workspace_init", workspace_init.as_str());
+    Ok(())
+}
+
+#[tokio::test]
+async fn build_inserts_workspace_init_params_from_storage() -> Result<(), Box<dyn std::error::Error>>
+{
+    let (_tempdir, workspace, graph, mut output) = test_setup().await?;
+    let _cmd_context = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_workspace_init(Some("workspace_init".to_string()))
+        .await?;
+
+    // Create another CmdContext, this time using no parameter.
+    let CmdContext { resources, .. } = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_workspace_init::<String>(None)
+        .await?;
+    let workspace_init_file = resources.borrow::<WorkspaceInitFile>();
+    let workspace_init = resources.borrow::<String>();
+
+    assert!(workspace_init_file.exists());
+    assert_eq!(
+        "workspace_init\n",
+        tokio::fs::read_to_string(&*workspace_init_file).await?
+    );
+    assert_eq!("workspace_init", workspace_init.as_str());
+    Ok(())
+}
+
+#[tokio::test]
+async fn build_inserts_profile_init_params_from_parameter() -> Result<(), Box<dyn std::error::Error>>
+{
+    let (_tempdir, workspace, graph, mut output) = test_setup().await?;
+
+    let CmdContext { resources, .. } = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_profile_init(Some("profile_init".to_string()))
+        .await?;
+    let profile_init_file = resources.borrow::<ProfileInitFile>();
+    let profile_init = resources.borrow::<String>();
+
+    assert!(profile_init_file.exists());
+    assert_eq!(
+        "profile_init\n",
+        tokio::fs::read_to_string(&*profile_init_file).await?
+    );
+    assert_eq!("profile_init", profile_init.as_str());
+    Ok(())
+}
+
+#[tokio::test]
+async fn build_inserts_profile_init_params_from_storage() -> Result<(), Box<dyn std::error::Error>>
+{
+    let (_tempdir, workspace, graph, mut output) = test_setup().await?;
+    let _cmd_context = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_profile_init(Some("profile_init".to_string()))
+        .await?;
+
+    // Create another CmdContext, this time using no parameter.
+    let CmdContext { resources, .. } = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_profile_init::<String>(None)
+        .await?;
+    let profile_init_file = resources.borrow::<ProfileInitFile>();
+    let profile_init = resources.borrow::<String>();
+
+    assert!(profile_init_file.exists());
+    assert_eq!(
+        "profile_init\n",
+        tokio::fs::read_to_string(&*profile_init_file).await?
+    );
+    assert_eq!("profile_init", profile_init.as_str());
+    Ok(())
+}
+
+#[tokio::test]
+async fn build_inserts_flow_init_params_from_parameter() -> Result<(), Box<dyn std::error::Error>> {
+    let (_tempdir, workspace, graph, mut output) = test_setup().await?;
+
+    let CmdContext { resources, .. } = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_flow_init(Some("flow_init".to_string()))
+        .await?;
+    let flow_init_file = resources.borrow::<FlowInitFile>();
+    let flow_init = resources.borrow::<String>();
+
+    assert!(flow_init_file.exists());
+    assert_eq!(
+        "flow_init\n",
+        tokio::fs::read_to_string(&*flow_init_file).await?
+    );
+    assert_eq!("flow_init", flow_init.as_str());
+    Ok(())
+}
+
+#[tokio::test]
+async fn build_inserts_flow_init_params_from_storage() -> Result<(), Box<dyn std::error::Error>> {
+    let (_tempdir, workspace, graph, mut output) = test_setup().await?;
+    let _cmd_context = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_flow_init(Some("flow_init".to_string()))
+        .await?;
+
+    // Create another CmdContext, this time using no parameter.
+    let CmdContext { resources, .. } = CmdContextBuilder::new(&workspace, &graph, &mut output)
+        .with_flow_init::<String>(None)
+        .await?;
+    let flow_init_file = resources.borrow::<FlowInitFile>();
+    let flow_init = resources.borrow::<String>();
+
+    assert!(flow_init_file.exists());
+    assert_eq!(
+        "flow_init\n",
+        tokio::fs::read_to_string(&*flow_init_file).await?
+    );
+    assert_eq!("flow_init", flow_init.as_str());
+    Ok(())
+}
+
+// Test fixture
+async fn test_setup() -> Result<
+    (
+        tempfile::TempDir,
+        Workspace,
+        ItemSpecGraph<VecCopyError>,
+        NoOpOutput,
+    ),
+    Box<dyn std::error::Error>,
+> {
+    let tempdir = tempfile::tempdir()?;
+    let workspace = Workspace::new(
+        WorkspaceSpec::Path(tempdir.path().into()),
+        profile!("test_profile"),
+        flow_id!("test_flow"),
+    )?;
+    let graph = {
+        let mut graph_builder = ItemSpecGraphBuilder::<VecCopyError>::new();
+        graph_builder.add_fn(VecCopyItemSpec.into());
+        graph_builder.build()
+    };
+
+    Ok((tempdir, workspace, graph, NoOpOutput))
 }
