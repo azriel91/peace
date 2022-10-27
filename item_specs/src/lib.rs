aae89b9eea18f887e84a38238182225e890f4109
Azriel Hoh <azriel91@gmail.com>
2022-10-27 20:10:10 +1300

    Add `peace_item_specs` crate.

---
+++ item_specs/src/lib.rs
@@ -0,0 +1,20 @@
+//! Collection of item specs the peace framework.
+//!
+//! Every item spec crate needs to be enabled with its own feature. Example:
+//!
+//! ```toml
+//! peace_item_specs = { version = "0.0.3", features = ["file_download"] }
+//! ```
+//!
+//! In code:
+//!
+//! ```rust
+//! use peace::cfg::{item_spec_id, ItemSpecId};
+//! use peace_item_specs::file_download::FileDownloadItemSpec;
+//!
+//! let file_download_item_spec = FileDownloadItemSpec::new(item_spec_id!("file_to_download"));
+//! ```
+
+// Re-exports
+#[cfg(feature = "file_download")]
+pub use peace_item_spec_file_download as file_download;
