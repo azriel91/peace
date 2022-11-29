5960ecda4371d8d5ee887da0bd660877027e1f05
Azriel Hoh <azriel91@gmail.com>
2022-11-30 08:06:47 +1300

    Run `state_diff_sh_cmd` in `ShCmdStateDiffFnSpec`.

--- item_specs/sh_cmd/src/sh_cmd_state_diff_fn_spec.rs
+++ item_specs/sh_cmd/src/sh_cmd_state_diff_fn_spec.rs
@@ -26,7 +26,7 @@ where
         state_current: &State<ShCmdState, ShCmdExecutionRecord>,
         state_desired: &ShCmdState,
     ) -> Result<Self::StateDiff, ShCmdError> {
-        let mut state_desired_sh_cmd = sh_cmd_data.sh_cmd_params().state_desired_sh_cmd().clone();
+        let mut state_diff_sh_cmd = sh_cmd_data.sh_cmd_params().state_diff_sh_cmd().clone();
 
         let state_current_arg = match &state_current.logical {
             ShCmdState::None => "",
@@ -36,10 +36,10 @@ where
             ShCmdState::None => "",
             ShCmdState::Some { stdout, .. } => stdout.as_ref(),
         };
-        state_desired_sh_cmd
+        state_diff_sh_cmd
             .arg(state_current_arg)
             .arg(state_desired_arg);
-        ShCmdExecutor::exec(&state_desired_sh_cmd)
+        ShCmdExecutor::exec(&state_diff_sh_cmd)
             .await
             .map(|state| match state.logical {
                 ShCmdState::None => ShCmdStateDiff::new(String::from(""), String::from("")),
