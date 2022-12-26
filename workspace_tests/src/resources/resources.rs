9a17b5168615c637c860ffed1be9ba6e2d56f976
Azriel Hoh <azriel91@gmail.com>
2022-12-26 18:53:33 +1300

    Add tests for `Resources`.

--- workspace_tests/src/resources/resources.rs
+++ workspace_tests/src/resources/resources.rs
@@ -1,6 +1,8 @@
+use std::any::{Any, TypeId};
+
 use peace::resources::{
     resources::ts::{
-        Cleaned, CleanedDry, Ensured, EnsuredDry, SetUp, WithStatesCurrent,
+        Cleaned, CleanedDry, Empty, Ensured, EnsuredDry, SetUp, WithStatesCurrent,
         WithStatesCurrentAndDesired, WithStatesCurrentDiffs, WithStatesDesired, WithStatesSaved,
         WithStatesSavedAndDesired, WithStatesSavedDiffs,
     },
@@ -10,6 +12,27 @@ use peace::resources::{
     },
     Resources,
 };
+
+#[test]
+fn debug() {
+    let mut resources = Resources::new();
+    resources.insert(1u32);
+
+    assert_eq!(
+        r#"Resources { inner: {u32: 1}, marker: PhantomData<peace_resources::resources::ts::Empty> }"#,
+        format!("{resources:?}")
+    );
+}
+
+#[test]
+fn defaults_to_resources_empty() {
+    let resources_default = Resources::default();
+
+    assert_eq!(
+        TypeId::of::<Resources::<Empty>>(),
+        resources_default.type_id()
+    );
+}
 
 #[test]
 fn resources_set_up_from_resources_empty() {
