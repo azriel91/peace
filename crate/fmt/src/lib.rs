82bf4a9ec293970345a6d4151b01d4b7d15da487
Azriel Hoh <azriel91@gmail.com>
2023-02-05 15:27:06 +1300

    Add `present` and `presentln` macros.

--- crate/fmt/src/lib.rs
+++ crate/fmt/src/lib.rs
@@ -16,3 +16,36 @@ mod owned_deserialize;
 mod presentable;
 mod presentable_list;
 mod presenter;
+
+/// Ergonomically present multiple [`Presentable`]s.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// use peace_fmt::{present, Presentable};
+///
+/// present!(output, "a str", item, "\n");
+/// ```
+#[macro_export]
+macro_rules! present {
+    ($output:ident, [$($p:expr),+]) => {
+        $($output.present($p).await?;)+
+    };
+}
+
+/// Ergonomically present multiple [`Presentable`]s.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// use peace_fmt::{present, Presentable};
+///
+/// presentln!(output, "a str", item, "\n");
+/// ```
+#[macro_export]
+macro_rules! presentln {
+    ($output:ident, [$($p:expr),+]) => {
+        $($output.present($p).await?;)+
+        $output.present("\n").await?;
+    };
+}
