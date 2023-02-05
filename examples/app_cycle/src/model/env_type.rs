b98528b9c982a6b3dbe548627fb05671a8f46401
Azriel Hoh <azriel91@gmail.com>
2023-02-05 15:34:52 +1300

    Use `output.present` instead of `eprintln` in `app_cycle` profile show.

--- examples/app_cycle/src/model/env_type.rs
+++ examples/app_cycle/src/model/env_type.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use peace::fmt::Presenter;
 use serde::{Deserialize, Serialize};
 
 use crate::model::EnvTypeParseError;
@@ -32,6 +33,19 @@ impl std::fmt::Display for EnvType {
         match self {
             EnvType::Development => "development".fmt(f),
             EnvType::Production => "production".fmt(f),
+        }
+    }
+}
+
+#[peace::fmt::async_trait(?Send)]
+impl peace::fmt::Presentable for EnvType {
+    async fn present<'output, PR>(&self, presenter: &mut PR) -> Result<(), PR::Error>
+    where
+        PR: Presenter<'output>,
+    {
+        match self {
+            EnvType::Development => presenter.code_inline("development").await,
+            EnvType::Production => presenter.code_inline("production").await,
         }
     }
 }
