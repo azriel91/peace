c6e244a69c637af6d5558ac45d419a679e984575
Azriel Hoh <azriel91@gmail.com>
2023-03-01 19:08:54 +1300

    Support new `CmdCtx` in `StatesSavedDisplayCmd`.

--- crate/rt/src/cmds/states_saved_display_cmd.rs
+++ crate/rt/src/cmds/states_saved_display_cmd.rs
@@ -1,5 +1,9 @@
 use std::{fmt::Debug, marker::PhantomData};
 
+use peace_cmd::{
+    ctx::{CmdCtx, CmdCtxView},
+    scopes::{SingleProfileSingleFlow, SingleProfileSingleFlowView},
+};
 use peace_resources::{
     resources::ts::{SetUp, WithStatesSaved},
     Resources,
@@ -19,6 +23,45 @@ where
     PKeys: ParamsKeys + 'static,
     O: OutputWrite<E>,
 {
+    /// Displays [`StatesSaved`]s from storage.
+    ///
+    /// Either [`StatesSavedDiscoverCmd`] or [`StatesDiscoverCmd`] must have
+    /// run prior to this command to read the state.
+    ///
+    /// [`StatesSavedDiscoverCmd`]: crate::StatesSavedDiscoverCmd
+    /// [`StatesDiscoverCmd`]: crate::StatesDiscoverCmd
+    pub async fn exec_v2(
+        mut cmd_ctx: CmdCtx<'_, O, SingleProfileSingleFlow<E, PKeys, SetUp>, PKeys>,
+    ) -> Result<CmdCtx<'_, O, SingleProfileSingleFlow<E, PKeys, WithStatesSaved>, PKeys>, E> {
+        let CmdCtxView { output, scope, .. } = cmd_ctx.view();
+        let SingleProfileSingleFlowView {
+            states_type_regs,
+            resources,
+            ..
+        } = scope.view();
+
+        let states_saved_result = StatesSavedReadCmd::<E, O, PKeys>::exec_internal(
+            resources,
+            states_type_regs.states_current_type_reg(),
+        )
+        .await;
+
+        match states_saved_result {
+            Ok(states_saved) => {
+                output.present(&states_saved).await?;
+
+                let cmd_ctx = cmd_ctx.resources_update(|resources| {
+                    Resources::<WithStatesSaved>::from((resources, states_saved))
+                });
+                Ok(cmd_ctx)
+            }
+            Err(e) => {
+                output.write_err(&e).await?;
+                Err(e)
+            }
+        }
+    }
+
     /// Displays [`StatesCurrent`]s from storage.
     ///
     /// Either [`StatesCurrentDiscoverCmd`] or [`StatesDiscoverCmd`] must have
