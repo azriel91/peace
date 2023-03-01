fc3d95ca9a2716f89be1e1df0a049c86b47fac86
Azriel Hoh <azriel91@gmail.com>
2023-03-01 19:13:35 +1300

    Support new `CmdCtx` in `StatesDiscoverCmd`.

--- crate/rt/src/cmds/states_discover_cmd.rs
+++ crate/rt/src/cmds/states_discover_cmd.rs
@@ -1,5 +1,9 @@
 use std::{fmt::Debug, marker::PhantomData};
 
+use peace_cmd::{
+    ctx::CmdCtx,
+    scopes::{SingleProfileSingleFlow, SingleProfileSingleFlowView},
+};
 use peace_resources::{
     resources::ts::{SetUp, WithStatesCurrentAndDesired},
     Resources,
@@ -16,6 +20,47 @@ where
     E: std::error::Error + From<Error> + Send + 'static,
     PKeys: ParamsKeys + 'static,
 {
+    /// Runs [`StateCurrentFnSpec`]` and
+    /// `[`StateDesiredFnSpec`]`::`[`try_exec`] for each [`ItemSpec`].
+    ///
+    /// At the end of this function, [`Resources`] will be populated with
+    /// [`StatesCurrent`] and [`StatesDesired`], and will be serialized to
+    /// `{profile_dir}/states.yaml` and `{profile_dir}/states_desired.yaml`.
+    ///
+    /// [`try_exec`]: peace_cfg::TryFnSpec::try_exec
+    /// [`ItemSpec`]: peace_cfg::ItemSpec
+    /// [`StatesCurrent`]: peace_resources::StatesCurrent
+    /// [`StatesDesired`]: peace_resources::StatesDesired
+    /// [`StateCurrentFnSpec`]: peace_cfg::ItemSpec::StateCurrentFnSpec
+    /// [`StateDesiredFnSpec`]: peace_cfg::ItemSpec::StateDesiredFnSpec
+    pub async fn exec_v2(
+        mut cmd_ctx: CmdCtx<'_, O, SingleProfileSingleFlow<E, PKeys, SetUp>, PKeys>,
+    ) -> Result<
+        CmdCtx<'_, O, SingleProfileSingleFlow<E, PKeys, WithStatesCurrentAndDesired>, PKeys>,
+        E,
+    > {
+        let SingleProfileSingleFlowView {
+            flow, resources, ..
+        } = cmd_ctx.scope_mut().view();
+        let item_spec_graph = flow.graph();
+
+        let states_current =
+            StatesCurrentDiscoverCmd::<E, O, PKeys>::exec_internal(item_spec_graph, resources)
+                .await?;
+        let states_desired =
+            StatesDesiredDiscoverCmd::<E, O, PKeys>::exec_internal(item_spec_graph, resources)
+                .await?;
+
+        let cmd_ctx = cmd_ctx.resources_update(|resources| {
+            Resources::<WithStatesCurrentAndDesired>::from((
+                resources,
+                states_current,
+                states_desired,
+            ))
+        });
+        Ok(cmd_ctx)
+    }
+
     /// Runs [`StateCurrentFnSpec`]` and
     /// `[`StateDesiredFnSpec`]`::`[`try_exec`] for each [`ItemSpec`].
     ///
