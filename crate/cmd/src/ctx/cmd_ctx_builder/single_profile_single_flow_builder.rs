39f65180d1eb887565f4a1337de518bc4a8e7674
Azriel Hoh <azriel91@gmail.com>
2023-03-02 15:52:59 +1300

    Remove `Clone, PartialEq, Eq` derives from scope builders.
    The `ParamsTypeRegBuilder`s don't implement these traits, and we don't
    need them.

--- crate/cmd/src/ctx/cmd_ctx_builder/single_profile_single_flow_builder.rs
+++ crate/cmd/src/ctx/cmd_ctx_builder/single_profile_single_flow_builder.rs
@@ -1,5 +1,5 @@
 /// Data stored by `CmdCtxBuilder` while building a
 /// `CmdCtx<SingleProfileSingleFlow>`.
 #[peace_code_gen::cmd_ctx_builder_impl]
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct SingleProfileSingleFlowBuilder;
