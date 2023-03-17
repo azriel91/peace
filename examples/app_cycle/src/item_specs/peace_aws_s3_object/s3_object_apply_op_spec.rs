a39ed1633a2bf2adf304df0fe52a39fd7e9c18bd
Azriel Hoh <azriel91@gmail.com>
2023-03-18 10:00:19 +1300

    Update `S3Object` item spec apply logic to include clean logic.

--- examples/app_cycle/src/item_specs/peace_aws_s3_object/s3_object_apply_op_spec.rs
+++ examples/app_cycle/src/item_specs/peace_aws_s3_object/s3_object_apply_op_spec.rs
@@ -26,7 +26,7 @@ where
 
     async fn check(
         _s3_object_data: S3ObjectData<'_, Id>,
-        _state_current: &S3ObjectState,
+        state_current: &S3ObjectState,
         _state_desired: &S3ObjectState,
         diff: &S3ObjectStateDiff,
     ) -> Result<OpCheckStatus, S3ObjectError> {
@@ -47,10 +47,28 @@ where
                 Ok(op_check_status)
             }
             S3ObjectStateDiff::Removed => {
-                panic!(
-                    "`S3ObjectApplyOpSpec::check` called with `S3ObjectStateDiff::Removed`.\n\
-                    An ensure should never remove a object."
-                );
+                let op_check_status = match state_current {
+                    S3ObjectState::None => OpCheckStatus::ExecNotRequired,
+                    S3ObjectState::Some {
+                        bucket_name: _,
+                        object_key: _,
+                        content_md5_hexstr: _,
+                        e_tag: _,
+                    } => {
+                        #[cfg(not(feature = "output_progress"))]
+                        {
+                            OpCheckStatus::ExecRequired
+                        }
+                        #[cfg(feature = "output_progress")]
+                        {
+                            let steps_required = 1;
+                            let progress_limit = ProgressLimit::Steps(steps_required);
+                            OpCheckStatus::ExecRequired { progress_limit }
+                        }
+                    }
+                };
+
+                Ok(op_check_status)
             }
             S3ObjectStateDiff::BucketNameModified {
                 bucket_name_current,
@@ -85,7 +103,7 @@ where
     async fn exec(
         _op_ctx: OpCtx<'_>,
         data: S3ObjectData<'_, Id>,
-        _state_current: &S3ObjectState,
+        state_current: &S3ObjectState,
         state_desired: &S3ObjectState,
         diff: &S3ObjectStateDiff,
     ) -> Result<S3ObjectState, S3ObjectError> {
@@ -172,22 +190,48 @@ where
                             .expect("Expected ETag to be some when put_object is successful.")
                             .to_string();
 
-                        let state_ensured = S3ObjectState::Some {
+                        let state_applied = S3ObjectState::Some {
                             bucket_name: bucket_name.clone(),
                             object_key: object_key.clone(),
                             content_md5_hexstr: Some(content_md5_hexstr.clone()),
                             e_tag: Generated::Value(e_tag),
                         };
 
-                        Ok(state_ensured)
+                        Ok(state_applied)
                     }
                 }
             }
             S3ObjectStateDiff::Removed => {
-                panic!(
-                    "`S3ObjectApplyOpSpec::exec` called with `S3ObjectStateDiff::Removed`.\n\
-                    An ensure should never remove a object."
-                );
+                match state_current {
+                    S3ObjectState::None => {}
+                    S3ObjectState::Some {
+                        bucket_name,
+                        object_key,
+                        content_md5_hexstr: _,
+                        e_tag: _,
+                    } => {
+                        let client = data.client();
+                        client
+                            .delete_object()
+                            .bucket(bucket_name)
+                            .key(object_key)
+                            .send()
+                            .await
+                            .map_err(|error| {
+                                let bucket_name = bucket_name.to_string();
+                                let object_key = object_key.to_string();
+
+                                S3ObjectError::S3ObjectDeleteError {
+                                    bucket_name,
+                                    object_key,
+                                    error,
+                                }
+                            })?;
+                    }
+                }
+
+                let state_applied = state_desired.clone();
+                Ok(state_applied)
             }
             S3ObjectStateDiff::InSyncExists | S3ObjectStateDiff::InSyncDoesNotExist => {
                 unreachable!(
