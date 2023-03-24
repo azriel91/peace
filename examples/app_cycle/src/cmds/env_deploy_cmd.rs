3a5596e6f35dd93997896b471094704a700a7618
Azriel Hoh <azriel91@gmail.com>
2023-03-24 15:39:20 +1300

    Present ensured states in order.

--- examples/app_cycle/src/cmds/env_deploy_cmd.rs
+++ examples/app_cycle/src/cmds/env_deploy_cmd.rs
@@ -1,5 +1,7 @@
 use futures::FutureExt;
 use peace::{
+    cmd::scopes::SingleProfileSingleFlowView,
+    fmt::presentable::{Heading, HeadingLevel, ListNumbered},
     rt::cmds::{sub::StatesSavedReadCmd, EnsureCmd},
     rt_model::output::OutputWrite,
 };
@@ -27,13 +29,44 @@ impl EnvDeployCmd {
             StatesSavedReadCmd::exec(ctx).boxed_local()
         })
         .await?;
-        EnvCmd::run_and_present(output, false, |ctx| {
+        EnvCmd::run(output, false, |ctx| {
             async move {
                 let states_saved_ref = &states_saved;
-                EnsureCmd::exec(ctx, states_saved_ref).await
+                let states_ensured = EnsureCmd::exec(ctx, states_saved_ref).await?;
+
+                let states_ensured_raw_map = &**states_ensured;
+
+                let SingleProfileSingleFlowView { output, flow, .. } = ctx.view();
+                let states_ensured_presentables = {
+                    let states_ensured_presentables = flow
+                        .graph()
+                        .iter_insertion()
+                        .map(|item_spec| {
+                            let item_spec_id = item_spec.id();
+                            match states_ensured_raw_map.get(item_spec_id) {
+                                Some(state_ensured) => (item_spec_id, format!(": {state_ensured}")),
+                                None => (item_spec_id, String::from(": <unknown>")),
+                            }
+                        })
+                        .collect::<Vec<_>>();
+
+                    ListNumbered::new(states_ensured_presentables)
+                };
+
+                output
+                    .present(&(
+                        Heading::new(HeadingLevel::Level1, "States Ensured"),
+                        states_ensured_presentables,
+                        "\n",
+                    ))
+                    .await?;
+
+                Ok(())
             }
             .boxed_local()
         })
-        .await
+        .await?;
+
+        Ok(())
     }
 }
