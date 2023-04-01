cfbfef72cb33a07352ba14814980bce98c90444e
Azriel Hoh <azriel91@gmail.com>
2023-04-01 20:23:51 +1300

    Revert "Move progress bar finish and abandon logic from `CliOutput` into `Progress`."
    This reverts commit 3e05e36c7238b7a284060115af2a18cc49346c0a.

--- crate/rt/src/progress.rs
+++ crate/rt/src/progress.rs
@@ -1,7 +1,7 @@
 use peace_cfg::{
     progress::{
-        ProgressComplete, ProgressDelta, ProgressMsgUpdate, ProgressStatus, ProgressTracker,
-        ProgressUpdate, ProgressUpdateAndId,
+        ProgressDelta, ProgressMsgUpdate, ProgressStatus, ProgressTracker, ProgressUpdate,
+        ProgressUpdateAndId,
     },
     ItemSpecId,
 };
@@ -48,12 +48,6 @@ impl Progress {
                 ProgressUpdate::Complete(progress_complete) => {
                     progress_tracker
                         .set_progress_status(ProgressStatus::Complete(progress_complete.clone()));
-
-                    let progress_bar = progress_tracker.progress_bar();
-                    match progress_complete {
-                        ProgressComplete::Success => progress_bar.finish(),
-                        ProgressComplete::Fail => progress_bar.abandon(),
-                    }
                 }
             }
 
