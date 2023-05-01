72069c4fc8511946fa549d4d00f135ac3edc2b85
Azriel Hoh <azriel91@gmail.com>
2023-05-02 09:48:13 +1200

    Reduce duplication in `impl_from_params_for_params_spec`.

--- crate/params_derive/src/impl_from_params_for_params_spec.rs
+++ crate/params_derive/src/impl_from_params_for_params_spec.rs
@@ -1,7 +1,6 @@
-use proc_macro2::Span;
 use syn::{
-    punctuated::Punctuated, DeriveInput, Fields, Ident, ImplGenerics, LitInt, Path, TypeGenerics,
-    Variant, WhereClause,
+    punctuated::Punctuated, DeriveInput, Fields, Ident, ImplGenerics, Path, TypeGenerics, Variant,
+    WhereClause,
 };
 
 use crate::util::{
@@ -24,17 +23,17 @@ pub fn impl_from_params_for_params_spec(
         syn::Data::Struct(data_struct) => {
             let fields = &data_struct.fields;
 
-            struct_fields_map_to_value(params_spec_name, fields, peace_params_path)
+            struct_fields_map_to_value(params_name, params_spec_name, fields, peace_params_path)
         }
         syn::Data::Enum(data_enum) => {
             let variants = &data_enum.variants;
 
-            variants_map_to_value(params_spec_name, params_name, variants, peace_params_path)
+            variants_map_to_value(params_name, params_spec_name, variants, peace_params_path)
         }
         syn::Data::Union(data_union) => {
             let fields = Fields::from(data_union.fields.clone());
 
-            struct_fields_map_to_value(params_spec_name, &fields, peace_params_path)
+            struct_fields_map_to_value(params_name, params_spec_name, &fields, peace_params_path)
         }
     };
 
@@ -51,69 +50,50 @@ pub fn impl_from_params_for_params_spec(
 }
 
 fn struct_fields_map_to_value(
+    params_name: &Ident,
     params_spec_name: &Ident,
     fields: &Fields,
     peace_params_path: &Path,
 ) -> proc_macro2::TokenStream {
+    let fields_deconstructed = fields_deconstruct(fields);
+    let fields_map_to_value = fields_map_to_value(fields, peace_params_path);
+
     match fields {
-        Fields::Named(fields_named) => {
+        Fields::Named(_fields_named) => {
             // Generates:
             //
-            // #params_spec_name {
-            //     field_1: #peace_params_path::ValueSpec::Value(params.field_1),
-            //     field_2: #peace_params_path::ValueSpec::Value(params.field_2),
+            // ```rust
+            // let #params_name {
+            //     field_1,
+            //     field_2,
             //     marker: PhantomData,
+            // } = params;
+            //
+            // #params_spec_name {
+            //     #fields_map_to_value
             // }
-
-            let fields_map_to_value = fields_named.named.iter().fold(
-                proc_macro2::TokenStream::new(),
-                |mut tokens, field| {
-                    if let Some(field_name) = field.ident.as_ref() {
-                        if is_phantom_data(&field.ty) {
-                            tokens.extend(quote! {
-                                #field_name: std::marker::PhantomData,
-                            });
-                        } else {
-                            tokens.extend(quote! {
-                                #field_name: #peace_params_path::ValueSpec::Value(params.#field_name),
-                            });
-                        }
-                    }
-                    tokens
-                },
-            );
-
+            // ```
             quote! {
+                let #params_name {
+                    #(#fields_deconstructed),*
+                } = params;
+
                 #params_spec_name {
                     #fields_map_to_value
                 }
             }
         }
-        Fields::Unnamed(fields_unnamed) => {
+        Fields::Unnamed(_fields_unnamed) => {
             // Generates:
             //
-            // #params_spec_name(#peace_params_path::ValueSpec::Value(params.0),
-            // #peace_params_path::ValueSpec::Value(params.1), PhantomData)
-            let fields_map_to_value = fields_unnamed.unnamed.iter().enumerate().fold(
-                proc_macro2::TokenStream::new(),
-                |mut tokens, (field_index, field)| {
-                    // Need to convert this to a `LitInt`,
-                    // because `quote` outputs the index as `0usize` instead of `0`
-                    let field_index = LitInt::new(&format!("{field_index}"), Span::call_site());
-
-                    if is_phantom_data(&field.ty) {
-                        tokens.extend(quote!(std::marker::PhantomData,));
-                    } else {
-                        tokens.extend(
-                            quote!(#peace_params_path::ValueSpec::Value(params.#field_index),),
-                        );
-                    }
-
-                    tokens
-                },
-            );
-
+            // ```rust
+            // let #params_name(#(#fields_deconstructed),*) = params;
+            //
+            // #params_spec_name(#fields_map_to_value)
+            // ```
             quote! {
+                let #params_name(#(#fields_deconstructed),*) = params;
+
                 #params_spec_name(#fields_map_to_value)
             }
         }
@@ -122,8 +102,8 @@ fn struct_fields_map_to_value(
 }
 
 fn variants_map_to_value(
-    params_spec_name: &Ident,
     params_name: &Ident,
+    params_spec_name: &Ident,
     variants: &Punctuated<Variant, Token![,]>,
     peace_params_path: &Path,
 ) -> proc_macro2::TokenStream {
@@ -181,19 +161,52 @@ fn variant_fields_map_to_value(
     fields: &Fields,
     peace_params_path: &Path,
 ) -> proc_macro2::TokenStream {
+    let fields_map_to_value = fields_map_to_value(fields, peace_params_path);
+    match fields {
+        Fields::Named(_fields_named) => {
+            // Generates:
+            //
+            // ```rust
+            // ParamsSpecName {
+            //     #fields_map_to_value
+            // }
+            // ```
+            quote! {
+                #params_spec_name::#variant_name {
+                    #fields_map_to_value
+                }
+            }
+        }
+        Fields::Unnamed(_fields_unnamed) => {
+            // Generates:
+            //
+            // ```rust
+            // ParamsSpecName(#fields_map_to_value)
+            // ```
+
+            quote! {
+                #params_spec_name::#variant_name(#fields_map_to_value)
+            }
+        }
+        Fields::Unit => quote!(Self::#variant_name),
+    }
+}
+
+fn fields_map_to_value(fields: &Fields, peace_params_path: &Path) -> proc_macro2::TokenStream {
     match fields {
         Fields::Named(fields_named) => {
             // Generates:
             //
-            // ParamsSpecName {
-            //     field_1: #peace_params_path::ValueSpec::Value(field_1),
-            //     field_2: #peace_params_path::ValueSpec::Value(field_2),
-            //     marker: PhantomData,
-            // }
+            // ```rust
+            // field_1: #peace_params_path::ValueSpec::Value(field_1),
+            // field_2: #peace_params_path::ValueSpec::Value(field_2),
+            // marker: PhantomData,
+            // ```
 
-            let fields_map_to_value = fields_named.named.iter().fold(
-                proc_macro2::TokenStream::new(),
-                |mut tokens, field| {
+            fields_named
+                .named
+                .iter()
+                .fold(proc_macro2::TokenStream::new(), |mut tokens, field| {
                     if let Some(field_name) = field.ident.as_ref() {
                         if is_phantom_data(&field.ty) {
                             tokens.extend(quote! {
@@ -206,43 +219,31 @@ fn variant_fields_map_to_value(
                         }
                     }
                     tokens
-                },
-            );
-
-            quote! {
-                #params_spec_name::#variant_name {
-                    #fields_map_to_value
-                }
-            }
+                })
         }
         Fields::Unnamed(fields_unnamed) => {
             // Generates:
             //
-            // ParamsSpecName(_0, #peace_params_path::ValueSpec::Value(_1), PhantomData)
-            let fields_map_to_value = fields_unnamed
-                .unnamed
-                .iter()
-                .enumerate()
-                .map(|(field_index, field)| (tuple_ident_from_field_index(field_index), field))
-                .fold(
-                    proc_macro2::TokenStream::new(),
-                    |mut tokens, (field_index, field)| {
-                        if is_phantom_data(&field.ty) {
-                            tokens.extend(quote!(std::marker::PhantomData,));
-                        } else {
-                            tokens.extend(
-                                quote!(#peace_params_path::ValueSpec::Value(#field_index),),
-                            );
-                        }
+            // ```rust
+            // #peace_params_path::ValueSpec::Value(_0),
+            // #peace_params_path::ValueSpec::Value(_1),
+            // PhantomData,
+            // ```
+            fields_unnamed.unnamed.iter().enumerate().fold(
+                proc_macro2::TokenStream::new(),
+                |mut tokens, (field_index, field)| {
+                    let field_name = tuple_ident_from_field_index(field_index);
 
-                        tokens
-                    },
-                );
+                    if is_phantom_data(&field.ty) {
+                        tokens.extend(quote!(std::marker::PhantomData,));
+                    } else {
+                        tokens.extend(quote!(#peace_params_path::ValueSpec::Value(#field_name),));
+                    }
 
-            quote! {
-                #params_spec_name::#variant_name(#fields_map_to_value)
-            }
+                    tokens
+                },
+            )
         }
-        Fields::Unit => quote!(Self::#variant_name),
+        Fields::Unit => quote!(),
     }
 }
