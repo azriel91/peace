f4a871d698d3f5919d2b3f64ea987875436083ef
Azriel Hoh <azriel91@gmail.com>
2023-05-13 13:19:13 +1200

    Reorganize `fields_map` logic.

--- crate/params_derive/src/fields_map.rs
+++ crate/params_derive/src/fields_map.rs
@@ -1,4 +1,4 @@
-use syn::{Attribute, Fields, Path, Type};
+use syn::{Attribute, Field, Fields, Path, Type};
 
 use crate::{
     util::{is_external, is_phantom_data, is_serde_bound_attr},
@@ -6,7 +6,8 @@ use crate::{
 };
 
 pub fn fields_to_optional(fields: &mut Fields) {
-    fields_map(fields, |field_ty| {
+    fields_map(fields, |field| {
+        let field_ty = &field.ty;
         if is_phantom_data(field_ty) {
             field_ty.clone()
         } else {
@@ -20,30 +21,30 @@ pub fn fields_to_optional(fields: &mut Fields) {
 /// If the type is marked with `#[params(external)]`, then it is wrapped as
 /// `ValueSpec<MyTypeWrapper>`.
 pub fn fields_to_value_spec(fields: &mut Fields, peace_params_path: &Path) {
-    // This has to come before `fields_map` because the attributes are cleared in
-    // it.
-    fields.iter_mut().for_each(|field| {
-        if is_external(&field.attrs) {
-            field.ty = ExternalType::wrapper_type(&field.ty);
-        }
-    });
-
-    fields_map(fields, |field_ty| {
+    fields_map(fields, |field| {
+        let field_ty = &field.ty;
         if is_phantom_data(field_ty) {
             field_ty.clone()
         } else {
-            parse_quote!(#peace_params_path::ValueSpecFieldless<#field_ty>)
+            if is_external(&field.attrs) {
+                let field_ty = ExternalType::wrapper_type(field_ty);
+                parse_quote!(#peace_params_path::ValueSpecFieldless<#field_ty>)
+            } else {
+                parse_quote!(#peace_params_path::ValueSpecFieldless<#field_ty>)
+            }
         }
     })
 }
 
 fn fields_map<F>(fields: &mut Fields, f: F)
 where
-    F: Fn(&Type) -> Type,
+    F: Fn(&Field) -> Type,
 {
     match fields {
         Fields::Named(fields_named) => {
             fields_named.named.iter_mut().for_each(|field| {
+                field.ty = f(&field);
+
                 // Don't copy across most attributes.
                 // The only attribute we copy across is `#[serde(bound = "..")]`
                 field.attrs = field
@@ -51,14 +52,12 @@ where
                     .drain(..)
                     .filter(is_serde_bound_attr)
                     .collect::<Vec<Attribute>>();
-                field.attrs.clear();
-
-                let field_ty = &field.ty;
-                field.ty = f(field_ty);
             });
         }
         Fields::Unnamed(fields_unnamed) => {
             fields_unnamed.unnamed.iter_mut().for_each(|field| {
+                field.ty = f(&field);
+
                 // Don't copy across most attributes.
                 // The only attribute we copy across is `#[serde(bound = "..")]`
                 field.attrs = field
@@ -66,10 +65,6 @@ where
                     .drain(..)
                     .filter(is_serde_bound_attr)
                     .collect::<Vec<Attribute>>();
-                field.attrs.clear();
-
-                let field_ty = &field.ty;
-                field.ty = f(field_ty);
             });
         }
         Fields::Unit => {}
