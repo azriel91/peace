4df9357970332d8bc5c5fbd0573cc3ea39d5bece
Azriel Hoh <azriel91@gmail.com>
2023-06-18 19:11:44 +1200

    Fix `StatesGoal::from(StatesGoalStored)` test to actually test that.

--- workspace_tests/src/resources/states_goal.rs
+++ workspace_tests/src/resources/states_goal.rs
@@ -1,6 +1,6 @@
-use peace::resources::states::{StatesCurrent, StatesCurrentStored};
+use peace::resources::states::{StatesGoal, StatesGoalStored};
 
 #[test]
 fn from_states_goal_stored() {
-    let _states_goal = StatesCurrent::from(StatesCurrentStored::new());
+    let _states_goal = StatesGoal::from(StatesGoalStored::new());
 }
