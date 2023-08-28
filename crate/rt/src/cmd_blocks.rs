7f174921a1d21b81056204732dc9e3fc8c4928bf
Azriel Hoh <azriel91@gmail.com>
2023-08-28 20:09:43 +1200

    Implement `StatesCurrentReadCmdBlock`.

--- crate/rt/src/cmd_blocks.rs
+++ crate/rt/src/cmd_blocks.rs
@@ -3,7 +3,9 @@
 //! [`Item`]: peace_cfg::Item
 
 pub use diff_cmd_block::DiffCmdBlock;
+pub use states_current_read_cmd_block::StatesCurrentReadCmdBlock;
 pub use states_discover_cmd_block::StatesDiscoverCmdBlock;
 
 mod diff_cmd_block;
+mod states_current_read_cmd_block;
 mod states_discover_cmd_block;
---
+++ crate/rt/src/cmd_blocks/states_current_read_cmd_block.rs
@@ -0,0 +1,121 @@
+use std::{fmt::Debug, marker::PhantomData};
+
+use peace_cfg::{FlowId, ItemId};
+use peace_cmd::scopes::SingleProfileSingleFlowView;
+use peace_cmd_rt::{async_trait, CmdBlock};
+use peace_resources::{
+    paths::{FlowDir, StatesCurrentFile},
+    resources::ts::SetUp,
+    states::StatesCurrentStored,
+    type_reg::untagged::{BoxDtDisplay, TypeReg},
+    Resources,
+};
+use peace_rt_model::{outcomes::CmdOutcome, params::ParamsKeys, Error, StatesSerializer, Storage};
+use tokio::sync::mpsc::UnboundedSender;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "output_progress")] {
+        use peace_cfg::progress::ProgressUpdateAndId;
+        use tokio::sync::mpsc::Sender;
+    }
+}
+
+/// Reads [`StatesCurrentStored`]s from storage.
+///
+/// Either [`StatesDiscoverCmdBlock::current`] or
+/// [`StatesDiscoverCmdBlock::current_and_goal`] must have run prior to this
+/// command to read the state.
+///
+/// [`StatesDiscoverCmd`]: crate::StatesDiscoverCmd
+#[derive(Debug)]
+pub struct StatesCurrentReadCmdBlock<E, PKeys>(PhantomData<(E, PKeys)>);
+
+impl<E, PKeys> StatesCurrentReadCmdBlock<E, PKeys>
+where
+    E: std::error::Error + From<Error> + Send + 'static,
+    PKeys: ParamsKeys + 'static,
+{
+    pub(crate) async fn deserialize_internal(
+        resources: &mut Resources<SetUp>,
+        states_type_reg: &TypeReg<ItemId, BoxDtDisplay>,
+    ) -> Result<StatesCurrentStored, E> {
+        let flow_id = resources.borrow::<FlowId>();
+        let flow_dir = resources.borrow::<FlowDir>();
+        let storage = resources.borrow::<Storage>();
+        let states_current_file = StatesCurrentFile::from(&*flow_dir);
+
+        let states_current_stored = StatesSerializer::deserialize_stored(
+            &flow_id,
+            &storage,
+            states_type_reg,
+            &states_current_file,
+        )
+        .await?;
+
+        drop(storage);
+        drop(flow_dir);
+        drop(flow_id);
+
+        resources.insert(states_current_file);
+
+        Ok(states_current_stored)
+    }
+}
+
+impl<E, PKeys> Default for StatesCurrentReadCmdBlock<E, PKeys> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+#[async_trait(?Send)]
+impl<E, PKeys> CmdBlock for StatesCurrentReadCmdBlock<E, PKeys>
+where
+    E: std::error::Error + From<Error> + Send + 'static,
+    PKeys: Debug + ParamsKeys + 'static,
+{
+    type Error = E;
+    type InputT = ();
+    type Outcome = StatesCurrentStored;
+    type OutcomeAcc = StatesCurrentStored;
+    type OutcomePartial = Result<StatesCurrentStored, E>;
+    type PKeys = PKeys;
+
+    fn outcome_acc_init(&self) -> Self::OutcomeAcc {
+        StatesCurrentStored::new()
+    }
+
+    fn outcome_from_acc(&self, outcome_acc: Self::OutcomeAcc) -> Self::Outcome {
+        outcome_acc
+    }
+
+    async fn exec(
+        &self,
+        _input: Box<Self::InputT>,
+        cmd_view: &mut SingleProfileSingleFlowView<'_, Self::Error, Self::PKeys, SetUp>,
+        outcomes_tx: &UnboundedSender<Self::OutcomePartial>,
+        #[cfg(feature = "output_progress")] _progress_tx: &Sender<ProgressUpdateAndId>,
+    ) {
+        let SingleProfileSingleFlowView {
+            states_type_reg,
+            resources,
+            ..
+        } = cmd_view;
+
+        let states_current_stored = Self::deserialize_internal(resources, states_type_reg).await;
+
+        outcomes_tx
+            .send(states_current_stored)
+            .expect("Failed to send `states_current_stored`.");
+    }
+
+    fn outcome_collate(
+        &self,
+        block_outcome: &mut CmdOutcome<Self::OutcomeAcc, Self::Error>,
+        outcome_partial: Self::OutcomePartial,
+    ) -> Result<(), Self::Error> {
+        let states_current_stored = outcome_partial?;
+        block_outcome.value = states_current_stored;
+        Ok(())
+    }
+}
