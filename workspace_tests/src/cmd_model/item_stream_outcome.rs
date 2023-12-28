98a5277f7a80959ccf7457addac83f9b1f45fab2
Azriel Hoh <azriel91@gmail.com>
2023-12-29 10:47:58 +1300

    Add tests for `ItemStreamOutcome`.

--- workspace_tests/src/cmd_model/item_stream_outcome.rs
+++ workspace_tests/src/cmd_model/item_stream_outcome.rs
@@ -1,7 +1,20 @@
 use peace::{
     cfg::{item_id, ItemId},
     cmd_model::ItemStreamOutcome,
+    rt_model::fn_graph::StreamOutcomeState,
 };
+
+#[test]
+fn map() {
+    let item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+
+    let item_stream_outcome = item_stream_outcome.map(|n| n + 1);
+
+    assert_eq!(
+        ItemStreamOutcome::finished_with(2u16, vec![item_id!("mock")]),
+        item_stream_outcome
+    );
+}
 
 #[test]
 fn replace() {
@@ -28,4 +41,69 @@ fn replace_with() {
         ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]),
         item_stream_outcome
     );
+}
+
+#[test]
+fn into_value() {
+    let item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+
+    let n = item_stream_outcome.into_value();
+
+    assert_eq!(1u16, n);
+}
+
+#[test]
+fn value() {
+    let item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+
+    let n = item_stream_outcome.value();
+
+    assert_eq!(1u16, *n);
+}
+
+#[test]
+fn value_mut() {
+    let mut item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+
+    *item_stream_outcome.value_mut() += 1;
+
+    assert_eq!(2u16, *item_stream_outcome.value());
+}
+
+#[test]
+fn state() {
+    let item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+
+    assert_eq!(StreamOutcomeState::Finished, item_stream_outcome.state());
+}
+
+#[test]
+fn item_ids_processed() {
+    let item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+
+    assert_eq!(
+        &[item_id!("mock")],
+        item_stream_outcome.item_ids_processed()
+    );
+}
+
+#[test]
+fn item_ids_not_processed() {
+    let mut item_stream_outcome = ItemStreamOutcome::finished_with(1u16, vec![item_id!("mock")]);
+    item_stream_outcome.item_ids_not_processed = vec![item_id!("mock_1")];
+
+    assert_eq!(
+        &[item_id!("mock_1")],
+        item_stream_outcome.item_ids_not_processed()
+    );
+}
+
+#[test]
+fn default() {
+    let item_stream_outcome = ItemStreamOutcome::<u16>::default();
+
+    assert_eq!(0u16, *item_stream_outcome.value());
+    assert_eq!(StreamOutcomeState::NotStarted, item_stream_outcome.state());
+    assert!(item_stream_outcome.item_ids_processed().is_empty());
+    assert!(item_stream_outcome.item_ids_not_processed().is_empty());
 }
