86ae10b8dda3a6ae353ed3847b00af22615a8abd
Azriel Hoh <azriel91@gmail.com>
2024-01-10 17:33:43 +1300

    Add `peace::cfg::` prefix to `id_newtype` macro usages.

--- crate/static_check_macros/src/lib.rs
+++ crate/static_check_macros/src/lib.rs
@@ -182,7 +182,7 @@ fn ensure_valid_id(
     if let Some(proposed_id) = proposed_id {
         if is_valid_id(proposed_id) {
             let ty_name = Ident::new(ty_name, Span::call_site());
-            quote!( #ty_name ::new_unchecked( #proposed_id ))
+            quote!( peace::cfg:: #ty_name ::new_unchecked( #proposed_id ))
         } else {
             let message = format!(
                 "\"{proposed_id}\" is not a valid `{ty_name}`.\n\
