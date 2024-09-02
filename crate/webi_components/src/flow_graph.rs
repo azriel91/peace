6a3cf37365e32679b61224016c66f4dab3f2f485
Azriel Hoh <azriel91@gmail.com>
2024-09-02 18:43:33 +1200

    Use `DotSvg` to render outcome graph.

--- crate/webi_components/src/flow_graph.rs
+++ crate/webi_components/src/flow_graph.rs
@@ -1,6 +1,6 @@
 use dot_ix::{
     model::{common::DotSrcAndStyles, info_graph::InfoGraph},
-    web_components::{DotSvg, FlexDiag},
+    web_components::DotSvg,
 };
 use leptos::{
     component, server_fn::error::NoCustomError, view, IntoView, ServerFnError, Signal, SignalGet,
@@ -10,23 +10,22 @@ use leptos::{
 /// Renders the flow graph.
 #[component]
 pub fn FlowGraph() -> impl IntoView {
+    view! {
+        <Transition fallback=move || view! { <p>"Loading graph..."</p> }>
+            <div class="flex items-center justify-center">
+                <ProgressGraph />
+                <OutcomeGraph />
+            </div>
+        </Transition>
+    }
+}
+
+#[component]
+pub fn ProgressGraph() -> impl IntoView {
     let progress_dot_resource = leptos::create_resource(
         || (),
         move |()| async move { progress_dot_graph().await.unwrap() },
     );
-
-    let outcome_info_graph_resource = leptos::create_resource(
-        || (),
-        move |()| async move { outcome_info_graph().await.unwrap() },
-    );
-    let outcome_info_graph = move || {
-        let outcome_info_graph =
-            Signal::from(move || outcome_info_graph_resource.get().unwrap_or_default());
-
-        view! {
-            <FlexDiag info_graph=outcome_info_graph />
-        }
-    };
 
     let progress_info_graph = move || {
         let progress_info_graph_and_dot_src_and_styles = progress_dot_resource
@@ -45,15 +44,42 @@ pub fn FlowGraph() -> impl IntoView {
     };
 
     view! {
-        <Transition fallback=move || view! { <p>"Loading graph..."</p> }>
-            <div class="flex items-center justify-center">
-                <DotSvg
-                    info_graph=progress_info_graph.into()
-                    dot_src_and_styles=progress_dot_graph.into()
-                />
-                {outcome_info_graph}
-            </div>
-        </Transition>
+        <DotSvg
+            info_graph=progress_info_graph.into()
+            dot_src_and_styles=progress_dot_graph.into()
+        />
+    }
+}
+
+#[component]
+pub fn OutcomeGraph() -> impl IntoView {
+    let outcome_info_graph_resource = leptos::create_resource(
+        || (),
+        move |()| async move { outcome_info_graph().await.unwrap() },
+    );
+
+    let outcome_info_graph = Signal::from(move || {
+        outcome_info_graph_resource
+            .get()
+            .map(|outcome_info_graph_and_dot_src_and_styles| {
+                outcome_info_graph_and_dot_src_and_styles.0
+            })
+            .unwrap_or_default()
+    });
+
+    let outcome_dot_graph = Signal::from(move || {
+        outcome_info_graph_resource
+            .get()
+            .map(|outcome_info_graph_and_dot_src_and_styles| {
+                outcome_info_graph_and_dot_src_and_styles.1
+            })
+    });
+
+    view! {
+        <DotSvg
+            info_graph=outcome_info_graph.into()
+            dot_src_and_styles=outcome_dot_graph.into()
+        />
     }
 }
 
@@ -76,7 +102,9 @@ pub async fn progress_dot_graph()
 
 /// Returns the graph representing item outcomes.
 #[leptos::server(endpoint = "/flow_graph")]
-pub async fn outcome_info_graph() -> Result<InfoGraph, ServerFnError<NoCustomError>> {
+pub async fn outcome_info_graph()
+-> Result<(InfoGraph, DotSrcAndStyles), ServerFnError<NoCustomError>> {
+    use dot_ix::{model::common::GraphvizDotTheme, rt::IntoGraphvizDotSrc};
     use peace_flow_model::FlowSpecInfo;
 
     let flow_spec_info = leptos::use_context::<FlowSpecInfo>().ok_or_else(|| {
@@ -84,5 +112,7 @@ pub async fn outcome_info_graph() -> Result<InfoGraph, ServerFnError<NoCustomErr
     })?;
 
     let outcome_info_graph = flow_spec_info.to_outcome_info_graph();
-    Ok(outcome_info_graph)
+    let dot_src_and_styles =
+        IntoGraphvizDotSrc::into(&outcome_info_graph, &GraphvizDotTheme::default());
+    Ok((outcome_info_graph, dot_src_and_styles))
 }
