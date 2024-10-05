ec4a686e6274c5d14c217f0670e335a395eda3e8
Azriel Hoh <azriel91@gmail.com>
2024-10-06 12:09:45 +1300

    Gate `ItemLocationState` and `ItemLocationStateInProgress` behind `"output_progress"` feature.

--- crate/item_model/src/lib.rs
+++ crate/item_model/src/lib.rs
@@ -12,8 +12,6 @@ pub use crate::{
     item_interactions_example::ItemInteractionsExample,
     item_location::ItemLocation,
     item_location_ancestors::ItemLocationAncestors,
-    item_location_state::ItemLocationState,
-    item_location_state_in_progress::ItemLocationStateInProgress,
     item_location_tree::ItemLocationTree,
     item_location_type::ItemLocationType,
     item_locations_combined::ItemLocationsCombined,
@@ -22,17 +20,26 @@ pub use crate::{
 #[cfg(feature = "item_locations_and_interactions")]
 pub use crate::item_locations_and_interactions::ItemLocationsAndInteractions;
 
+#[cfg(feature = "output_progress")]
+pub use crate::{
+    item_location_state::ItemLocationState,
+    item_location_state_in_progress::ItemLocationStateInProgress,
+};
+
 mod item_interaction;
 mod item_interactions_current;
 mod item_interactions_current_or_example;
 mod item_interactions_example;
 mod item_location;
 mod item_location_ancestors;
-mod item_location_state;
-mod item_location_state_in_progress;
 mod item_location_tree;
 mod item_location_type;
 mod item_locations_combined;
+
+#[cfg(feature = "output_progress")]
+mod item_location_state;
+#[cfg(feature = "output_progress")]
+mod item_location_state_in_progress;
 
 #[cfg(feature = "item_locations_and_interactions")]
 mod item_locations_and_interactions;
