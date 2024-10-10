55f00d55d34b408745b63f210429ce73f83dc91b
Azriel Hoh <azriel91@gmail.com>
2024-10-10 15:27:44 +1300

    Send both progress and outcome `InfoGraph`s in one web request.

--- crate/webi_components/src/flow_graph_current.rs
+++ crate/webi_components/src/flow_graph_current.rs
@@ -17,47 +17,14 @@ use leptos::{
 ///   component that will render values about which node is selected.
 #[component]
 pub fn FlowGraphCurrent() -> impl IntoView {
-    view! {
-        <div class="flex items-center justify-center">
-            <ProgressGraph />
-            <OutcomeGraph />
-        </div>
-    }
-}
-
-#[server]
-async fn progress_info_graph_fetch() -> Result<InfoGraph, ServerFnError> {
-    use std::sync::{Arc, Mutex};
-
-    use peace_cmd_model::CmdExecutionId;
-    use peace_webi_model::FlowProgressInfoGraphs;
-
-    let cmd_execution_id = leptos::use_context::<Arc<Mutex<Option<CmdExecutionId>>>>();
-    let flow_progress_info_graphs = leptos::use_context::<FlowProgressInfoGraphs<CmdExecutionId>>();
-    let progress_info_graph = if let Some((cmd_execution_id, flow_progress_info_graphs)) =
-        cmd_execution_id.zip(flow_progress_info_graphs)
-    {
-        let cmd_execution_id = cmd_execution_id.lock().ok().as_deref().copied().flatten();
-        let flow_progress_info_graphs = flow_progress_info_graphs.lock().ok();
-
-        cmd_execution_id
-            .zip(flow_progress_info_graphs)
-            .and_then(|(cmd_execution_id, flow_progress_info_graphs)| {
-                flow_progress_info_graphs.get(&cmd_execution_id).cloned()
-            })
-            .unwrap_or_else(InfoGraph::default)
-    } else {
-        InfoGraph::default()
-    };
-
-    Ok(progress_info_graph)
-}
-
-#[component]
-fn ProgressGraph() -> impl IntoView {
     let (progress_info_graph_get, progress_info_graph_set) =
         leptos::create_signal(InfoGraph::default());
-    let (dot_src_and_styles, dot_src_and_styles_set) = leptos::create_signal(None);
+    let (progress_dot_src_and_styles, progress_dot_src_and_styles_set) =
+        leptos::create_signal(None);
+
+    let (outcome_info_graph_get, outcome_info_graph_set) =
+        leptos::create_signal(InfoGraph::default());
+    let (outcome_dot_src_and_styles, outcome_dot_src_and_styles_set) = leptos::create_signal(None);
 
     leptos::create_local_resource(
         move || (),
@@ -65,72 +32,20 @@ fn ProgressGraph() -> impl IntoView {
             use gloo_timers::future::TimeoutFuture;
 
             loop {
-                let progress_info_graph = progress_info_graph_fetch().await.unwrap_or_default();
-                let dot_src_and_styles =
+                let (progress_info_graph, outcome_info_graph) =
+                    info_graphs_fetch().await.unwrap_or_default();
+
+                // Progress
+                let progress_dot_src_and_styles =
                     IntoGraphvizDotSrc::into(&progress_info_graph, &GraphvizDotTheme::default());
 
                 if progress_info_graph != progress_info_graph_get.get_untracked() {
                     progress_info_graph_set.set(progress_info_graph);
-                    dot_src_and_styles_set.set(Some(dot_src_and_styles));
+                    progress_dot_src_and_styles_set.set(Some(progress_dot_src_and_styles));
                 }
 
-                TimeoutFuture::new(250).await;
-            }
-        },
-    );
-
-    view! {
-        <Transition fallback=move || view! { <p>"Loading graph..."</p> }>
-            <DotSvg
-                info_graph=progress_info_graph_get.into()
-                dot_src_and_styles=dot_src_and_styles.into()
-            />
-        </Transition>
-    }
-}
-
-#[server]
-async fn outcome_info_graph_fetch() -> Result<InfoGraph, ServerFnError> {
-    use std::sync::{Arc, Mutex};
-
-    use peace_cmd_model::CmdExecutionId;
-    use peace_webi_model::FlowOutcomeInfoGraphs;
-
-    let cmd_execution_id = leptos::use_context::<Arc<Mutex<Option<CmdExecutionId>>>>();
-    let flow_outcome_info_graphs = leptos::use_context::<FlowOutcomeInfoGraphs<CmdExecutionId>>();
-    let outcome_info_graph = if let Some((cmd_execution_id, flow_outcome_info_graphs)) =
-        cmd_execution_id.zip(flow_outcome_info_graphs)
-    {
-        let cmd_execution_id = cmd_execution_id.lock().ok().as_deref().copied().flatten();
-        let flow_outcome_info_graphs = flow_outcome_info_graphs.lock().ok();
-
-        cmd_execution_id
-            .zip(flow_outcome_info_graphs)
-            .and_then(|(cmd_execution_id, flow_outcome_info_graphs)| {
-                flow_outcome_info_graphs.get(&cmd_execution_id).cloned()
-            })
-            .unwrap_or_else(InfoGraph::default)
-    } else {
-        InfoGraph::default()
-    };
-
-    Ok(outcome_info_graph)
-}
-
-#[component]
-fn OutcomeGraph() -> impl IntoView {
-    let (outcome_info_graph_get, outcome_info_graph_set) =
-        leptos::create_signal(InfoGraph::default());
-    let (dot_src_and_styles, dot_src_and_styles_set) = leptos::create_signal(None);
-
-    leptos::create_local_resource(
-        move || (),
-        move |()| async move {
-            use gloo_timers::future::TimeoutFuture;
-
-            loop {
-                let outcome_info_graph = outcome_info_graph_fetch().await.unwrap_or_default();
-                let dot_src_and_styles =
+                // Outcome
+                let outcome_dot_src_and_styles =
                     IntoGraphvizDotSrc::into(&outcome_info_graph, &GraphvizDotTheme::default());
 
                 if outcome_info_graph != outcome_info_graph_get.get_untracked() {
@@ -141,7 +56,7 @@ fn OutcomeGraph() -> impl IntoView {
                     }
 
                     outcome_info_graph_set.set(outcome_info_graph);
-                    dot_src_and_styles_set.set(Some(dot_src_and_styles));
+                    outcome_dot_src_and_styles_set.set(Some(outcome_dot_src_and_styles));
                 }
 
                 TimeoutFuture::new(250).await;
@@ -150,11 +65,57 @@ fn OutcomeGraph() -> impl IntoView {
     );
 
     view! {
-        <Transition fallback=move || view! { <p>"Loading graph..."</p> }>
-            <DotSvg
-                info_graph=outcome_info_graph_get.into()
-                dot_src_and_styles=dot_src_and_styles.into()
-            />
-        </Transition>
+        <div class="flex items-center justify-center">
+            <Transition fallback=move || view! { <p>"Loading graph..."</p> }>
+                <DotSvg
+                    info_graph=progress_info_graph_get.into()
+                    dot_src_and_styles=progress_dot_src_and_styles.into()
+                />
+                <DotSvg
+                    info_graph=outcome_info_graph_get.into()
+                    dot_src_and_styles=outcome_dot_src_and_styles.into()
+                />
+            </Transition>
+        </div>
+    }
+}
+
+#[server]
+async fn info_graphs_fetch() -> Result<(InfoGraph, InfoGraph), ServerFnError> {
+    use std::sync::{Arc, Mutex};
+
+    use peace_cmd_model::CmdExecutionId;
+    use peace_webi_model::{FlowOutcomeInfoGraphs, FlowProgressInfoGraphs};
+
+    let cmd_execution_id = leptos::use_context::<Arc<Mutex<Option<CmdExecutionId>>>>();
+    let flow_progress_info_graphs = leptos::use_context::<FlowProgressInfoGraphs<CmdExecutionId>>();
+    let flow_outcome_info_graphs = leptos::use_context::<FlowOutcomeInfoGraphs<CmdExecutionId>>();
+
+    if let Some(((cmd_execution_id, flow_progress_info_graphs), flow_outcome_info_graphs)) =
+        cmd_execution_id
+            .zip(flow_progress_info_graphs)
+            .zip(flow_outcome_info_graphs)
+    {
+        let cmd_execution_id = cmd_execution_id.lock().ok().as_deref().copied().flatten();
+        let flow_progress_info_graphs = flow_progress_info_graphs.lock().ok();
+
+        let progress_info_graph = cmd_execution_id
+            .zip(flow_progress_info_graphs)
+            .and_then(|(cmd_execution_id, flow_progress_info_graphs)| {
+                flow_progress_info_graphs.get(&cmd_execution_id).cloned()
+            })
+            .unwrap_or_else(InfoGraph::default);
+
+        let flow_outcome_info_graphs = flow_outcome_info_graphs.lock().ok();
+        let outcome_info_graph = cmd_execution_id
+            .zip(flow_outcome_info_graphs)
+            .and_then(|(cmd_execution_id, flow_outcome_info_graphs)| {
+                flow_outcome_info_graphs.get(&cmd_execution_id).cloned()
+            })
+            .unwrap_or_else(InfoGraph::default);
+
+        Ok((progress_info_graph, outcome_info_graph))
+    } else {
+        Ok((InfoGraph::default(), InfoGraph::default()))
     }
 }
